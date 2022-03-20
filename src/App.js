ab3b599418145dbe5acfe200e24239085d4f1bce
Ev1son <evison.pacheco21@gmail.com>
2022-03-20 18:45:30 -0300

    bugfix: função isLoading corrigida no botão

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     const [error666, setError666] = useState(0)
 
     function clearResults() {
-        /* ??? */
+        <div></div>
     }
 
     const handleSearch = () => {
@@ -60,8 +60,7 @@ function App() {
                     onChange={(e) => setCity(e.target.value)}
                 />
                 <button class="main__button" onClick={handleSearch}>
-                    {isLoading ? <div class="spinner-border text-light" role="status"></div>
-                        : "Pesquisar"}
+                    {isLoading ? "Pesquisando" : "Pesquisar"}
                 </button>
 
                 {weatherForecast ? (
@@ -88,13 +87,13 @@ function App() {
 
                 {error400 ? (
                     <div class="results">
-                        <h3>Cidade não encontrada</h3>
+                        <h3 class="results__title">Cidade não encontrada</h3>
                     </div>
                 ) : null}
 
                 {error666 ? (
                     <div class="results">
-                        <h3>Por favor preencha uma cidade</h3>
+                        <h3 class="results__title">Por favor preencha uma cidade</h3>
                     </div>
                 ) : null}
             </main>
